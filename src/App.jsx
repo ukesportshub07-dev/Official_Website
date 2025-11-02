b3e382fecfbe8efbc5872e41053585a83d8749cc
ukesportshub07-dev <ukesportshub07@gmail.com>
2025-11-03 01:55:44 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,28 @@
-import React, { useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './index.css';
 
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
+
 import Home from './home.jsx';
 import NewPage from './2ndpge.jsx';
 import Form from './Form.jsx';
 
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-};
-
 function App() {
   return (
-    <div className="bg-gray-900 text-gray-100 leading-relaxed min-h-screen">
-      
+    <div className="bg-gray-900 text-gray-100 leading-relaxed">
 
       <main>
-        <ScrollToTop /> 
-        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new-page" element={<NewPage />} />
-          <Route path="/forms" element={<Form />} /> 
+          <Route path="/Forms" element={<Form />} />
           <Route path="*" element={<h1 className="p-10 text-4xl text-red-500">404 - Page Not Found</h1>} />
         </Routes>
       </main>
+
 
     </div>
   );
