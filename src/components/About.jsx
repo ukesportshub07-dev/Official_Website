34c2397d950be2421177b53bd176677247865d07
ukesportshub07-dev <ukesportshub07@gmail.com>
2025-11-03 02:27:24 +0530

    About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,7 @@ function About() {
           </div>
         </div>
         
-        <aside className="p-8 m-6 rounded-lg mt-15 w-96 h-96 ">
+        <aside className="p-6 m-6 rounded-lg mt-15 w-96 h-96 ">
           <motion.img 
             initial={{ y: 0 }} 
             animate="float"
@@ -63,4 +63,5 @@ function About() {
 
 
 export default About;
+
 
