b7cbbea42d9d010f6b3aec1688b286035837038f
ukesportshub07-dev <ukesportshub07@gmail.com>
2025-11-02 14:34:55 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <a href="#" className="flex items-center gap-3">
-<img src="public/slideshow/whitelogo.png" 
+<img src="/slideshow/whitelogo.png" 
   alt="UKESPORTS HUB" className="h-12 w-12 rounded-md object-cover" onError={(e) => { e.target.src = 'https://via.placeholder.com/80' }}
 
             />
@@ -58,6 +58,7 @@ function Navbar() {
 
 
 export default Navbar;
+
 
 
 
