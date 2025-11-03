78aa903d94c1235a13f3997d3ff8287733e44a2c
ukesportshub07-dev <ukesportshub07@gmail.com>
2025-11-04 02:26:01 +0530

    Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -62,7 +62,7 @@ function Team() {
             isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'
           } hover:scale-105 hover:bg-gray-700 hover:shadow-lg hover:shadow-purple-500/30 cursor-pointer`}
         >
-          <img src="/images/udit.jpgtext=US" alt="US" className="mx-auto rounded-full h-28 w-28 object-cover border-2 border-purple-500/50" />
+          <img src="/images/udit.jpg" alt="US" className="mx-auto rounded-full h-28 w-28 object-cover border-2 border-purple-500/50" />
           <h4 className="mt-3 font-semibold text-white">Udit Samant</h4>
           <p className="text-xs text-gray-400">Technical Lead</p>
         </div>
@@ -114,6 +114,7 @@ function Team() {
 
 
 export default Team;
+
 
 
 
