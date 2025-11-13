f6be837a2323e746ff803938583f59ab593ab7cb
ukesportshub07-dev <ukesportshub07@gmail.com>
2025-11-13 16:47:51 +0530

    Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -19,7 +19,25 @@ function Team() {
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   };
 
   const handleCardClick = (member) => {
