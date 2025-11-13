3283fd084d3d0fc530b39ccea9e8db5b831082f6
ukesportshub07-dev <ukesportshub07@gmail.com>
2025-11-13 22:02:08 +0530

    Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -11,7 +11,7 @@ const coreTeam = [
 ];
 
 const ambassadors = [
-  { name: "", role: "Lead Ambassador", img: "", alt: "a" },
+  { name: "Dikshita Maindola", role: "D.A.V. Post Graduate College Dehradun", img: "/images/Ambasadors/IMG_20250809_115122.webp", alt: "Dikshita Maindola" },
   { name: "", role: "Outreach Lead", img: "", alt: "b" },
   { name: "", role: "Event Coordinator", img: "", alt: "c" },
   { name: "", role: "Community Manager", img: "", alt: "d" },
@@ -232,3 +232,4 @@ function Team() {
 }
 
 export default Team;
+
