5d9a6098feec82d2ccff5e777bcb81a76ce17977
UK Esports Hub <ukesportshub07@gmail.com>
2025-11-16 15:58:11 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -219,7 +219,7 @@ function Navbar() {
                   onClick={toggleBlogDropdown}
                   aria-expanded={isBlogOpen}
                 >
-                  Blog
+               Blogs / Updates
                   <svg className={`ml-1 w-4 h-4 transform transition-transform ${isBlogOpen ? 'rotate-180' : 'rotate-0'}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg>
                 </button>
 
@@ -437,6 +437,7 @@ function Navbar() {
 }
 
 export default Navbar;
+
 
 
 
