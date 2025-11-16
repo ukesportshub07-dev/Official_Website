cda90e5e6b34c7e0a879e9bb3a5bd39bd9b2e871
UK Esports Hub <ukesportshub07@gmail.com>
2025-11-17 00:29:30 +0530

    Add files via upload

---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,20 @@
+
+
+import React from 'react';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import GalleryComp from '../components/Gallery';
+
+
+function Gallery() {
+  return (
+   <>
+   <Navbar/>
+  <GalleryComp/>
+    <Footer/>
+   </>
+    
+  );
+}
+
+export default Gallery;
