3423e04e00cd7e3c940e1b193e218ff3e197de89
UK Esports Hub <ukesportshub07@gmail.com>
2025-11-21 19:44:44 +0530

    Add files via upload

---
+++ src/components/Sponsers.jsx
@@ -0,0 +1,157 @@
+import React from 'react';
+
+const imageData = [
+    { id: 1, src: '/logos/social_logo.png', alt: "logo", title: "S One" },
+    { id: 2, src: '/logos/whitelogo.png', alt: "logo", title: "S Two" },
+    { id: 3, src: '/images/image3.jpg', alt: "", title: "S Three" },
+    { id: 4, src: '/images/image4.jpg', alt: "", title: "S Four" },
+];
+
+const SponserComp = () => (
+    <div className='bg-grey-800'>
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Title Partners
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+        <div className="flex justify-center space-x-6 max-w-7xl mx-auto">
+
+            {imageData.map((item) => (
+
+                <div
+                    key={item.id}
+                    className="flex-1 overflow-hidden rounded-xl shadow-xl 
+               "
+                >
+                    <img
+                        src={item.src}
+                        alt={item.alt}
+                        className="w-full h-48 object-cover border-none"
+                    />
+
+                    <div className="p-4 text-center">
+                        <h3 className="text-xl font-semibold text-white">
+                            {item.title}
+                        </h3>
+                    </div>
+                </div>
+            ))}
+
+        </div>
+
+
+
+
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Powered By
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Venue Partner
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Gaming Gear Partner
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Energy Drink Partner
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+
+
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Streaming / Broadcast Partner
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+
+
+
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Hospitality Partner
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+
+
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Merchandise Partner
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+
+
+
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Media Partner
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+
+
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Gifting Partner
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+
+
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Supporting Partner
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+
+        <div className={`flex items-center w-full my-6 px-12`}>
+            <div className={`flex-grow border-t border-white`} />
+            <span className="flex-shrink-0 px-4 text-lg  text-white font-bold">
+                Representing 12 Universities of Uttarakhand
+            </span>
+            <div className={`flex-grow border-t border-white`} />
+        </div>
+
+    </div>
+);
+
+export default SponserComp;
