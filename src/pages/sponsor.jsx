b6d7bd19199638e26bbdbcf034f8b276c3fb0e2e
UK Esports Hub <ukesportshub07@gmail.com>
2025-11-21 19:46:09 +0530

    sponsor.jsx

--- src/pages/sponsor.jsx
+++ src/pages/sponsor.jsx
@@ -3,14 +3,14 @@
 import React from 'react';
 
 import Footer from '../components/Footer';
-
+import SponserComp from '../components/Sponsers';
 import Navbar from '../components/Navbar';
 
 function Oursponsors() {
   return (
    <>
    <Navbar/>
-   
+   <SponserComp/>
     <Footer/>
    </>
     
