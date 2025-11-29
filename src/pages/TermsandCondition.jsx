d71c2fe40bba9c114232721bbd7843236a9c7d36
UK Esports Hub <ukesportshub07@gmail.com>
2025-11-30 02:04:50 +0530

    TermsandCondition.jsx

--- src/pages/TermsandCondition.jsx
+++ src/pages/TermsandCondition.jsx
@@ -1,18 +1,32 @@
-// src/components/oursponsors.jsx
 
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import TermsandConditions from '../components/TermsandConditions';
 
 function TermsandCondition() {
   return (
-   <>
-   <Navbar/>
-   <TermsandConditions/>
-    <Footer/>
-   </>
-    
+    <>
+      <Helmet>
+        <title>Uttarakhand Esports Hub | Terms & Conditions</title>
+
+        <meta
+          name="description"
+          content="Read the official Terms and Conditions for participating in Uttarakhand Esports Hub tournaments, events, and esports competitions. Understand eligibility, rules, and guidelines."
+        />
+
+        <meta
+          name="keywords"
+          content="uttarakhand esports terms, uk esports rules, uk esports terms and conditions, tournament eligibility uk esports,uk esports gaming event rules"
+        />
+      </Helmet>
+
+      <Navbar />
+      <TermsandConditions />
+      <Footer />
+    </>
   );
 }
 
