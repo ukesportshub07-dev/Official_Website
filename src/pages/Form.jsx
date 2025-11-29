788d4b2ba667cb480a5b8f605c555017c611a3ec
UK Esports Hub <ukesportshub07@gmail.com>
2025-11-30 01:58:47 +0530

    Form.jsx

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,18 +1,32 @@
-// src/components/NewPage.jsx
 
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Formscreen from '../components/Formscreen';
 
-
 function Form() {
   return (
-   <>
-   <Navbar/>
-   <Formscreen/>
-   </>
-    
+    <>
+      <Helmet>
+        <title>Uk Esports Hub | Event Registration</title>
+
+        <meta
+          name="description"
+          content="Register now for Uttarakhand Esports Hub tournaments and events. Fill out the official registration form for BGMI, Valorant, Free Fire, and upcoming esports competitions."
+        />
+
+        <meta
+          name="keywords"
+          content="uttarakhand esports registration, uk esports form, free fire registration uttarakhand, valorant registration dehradun, esports registration form"
+        />
+      </Helmet>
+
+      <Navbar />
+      <Formscreen />
+      <Footer />
+    </>
   );
 }
 
