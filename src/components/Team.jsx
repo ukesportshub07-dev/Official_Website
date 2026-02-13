92d07e02bbe4f8927b343d390f4ebafafb8de219
UK Esports Hub <ukesportshub07@gmail.com>
2026-02-13 17:17:16 +0530

    Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef, useCallback } from 'react';
 
 const coreTeam = [
   { name: "Rahul Singh Dhami", role: "Founder / Co-Founder", img: "/images/Ambasadors/replace.png", alt: "Rahul Singh Dhami" },
-  { name: "Paras Bisht", role: "Chief Executive Officer", img: "public/images/paras.jpg", alt: "Paras Bisht" },
+  { name: "Paras Bisht", role: "Chief Executive Officer", img: "images/paras.jpg", alt: "Paras Bisht" },
   { name: "Udit Samant", role: "Technical Lead", img: "/images/udit.jpg", alt: "Udit Samant" },
   { name: "N/A", role: "Public Relations \n and Outreach ", img: "/images/Ambasadors/replace.png", alt: "N/A" },
   { name: "Shantanu", role: "General Manager", img: "/images/shantanu.jpg", alt: "Shantanu" },
@@ -219,6 +219,7 @@ function Team() {
 }
 
 export default Team;
+
 
 
 
