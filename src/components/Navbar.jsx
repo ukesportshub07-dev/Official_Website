e6482d3a954764511bcba1105b4e95dfd81322ba
Paras Bisht <96820300+ParasBisht007@users.noreply.github.com>
2026-02-17 00:18:35 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -98,7 +98,7 @@ function Navbar() {
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
-            <HashLink to="/" className="flex items-center gap-3"
+            <HashLink to="https://www.ukesportshub.in/" className="flex items-center gap-3"
               scroll={scrollWithOffset}
               onClick={handleLinkClick}>
               <img
@@ -273,4 +273,5 @@ function Navbar() {
 
 
 export default Navbar;
+
 
