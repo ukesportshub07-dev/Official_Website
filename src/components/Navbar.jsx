c158253d84dd602add0584c41f2d67eabf67fb47
Udit Samant_07 <118661713+Udit-07@users.noreply.github.com>
2026-02-17 00:15:39 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -98,7 +98,7 @@ function Navbar() {
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
-            <HashLink to="/#home" className="flex items-center gap-3"
+            <HashLink to="/" className="flex items-center gap-3"
               scroll={scrollWithOffset}
               onClick={handleLinkClick}>
               <img
@@ -273,3 +273,4 @@ function Navbar() {
 
 
 export default Navbar;
+
