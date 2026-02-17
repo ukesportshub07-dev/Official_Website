5d0f1344d33a242d3e3b97dee4bbb8b1d3c3bdb7
Paras Bisht <96820300+ParasBisht007@users.noreply.github.com>
2026-02-17 22:21:19 +0530

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,13 +17,6 @@ const formattedDate = currentDate.toLocaleDateString('en-GB', options);
       <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
       <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line>
     </svg>
-  const WhatsappIcon = (
-  <svg xmlns="http://www.w3.org/2000/svg"width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-      <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-      <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-      <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line>
-    </svg>
-
   );
 
 //    const scrollWithDelay = (el) => {
@@ -64,15 +57,7 @@ const formattedDate = currentDate.toLocaleDateString('en-GB', options);
               className="text-gray-400 hover:text-pink-500 transition duration-300" >
               {InstagramIcon}
                
-            </a>
-            <a href="https://chat.whatsapp.com/BjBsBIji9py0vjXhUz4dig"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="UK Esports Hub WhatsApp"
-                 className="text-gray-400 hover:text-green-500 transition duration-300"
-                                >
-        <WhatsappIcon />
-            </a>
+            </a>   
           </div>
         </div>
 
@@ -112,6 +97,7 @@ const formattedDate = currentDate.toLocaleDateString('en-GB', options);
 }
 
 export default Footer;
+
 
 
 
