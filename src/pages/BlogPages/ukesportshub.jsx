6837de3f5944291a8ac8deb8f302f265afc5efd7
Udit Samant_07 <118661713+Udit-07@users.noreply.github.com>
2026-02-21 01:44:18 +0530

    ukesportshub.jsx

--- src/pages/BlogPages/ukesportshub.jsx
+++ src/pages/BlogPages/ukesportshub.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar";
 
 const Ukesportshub = () => {
   const [views, setViews] = useState(0);
