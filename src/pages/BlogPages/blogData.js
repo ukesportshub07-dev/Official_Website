64ec8edf82ccb07b6c804afb94b93ea6e7414488
Paras Bisht <96820300+ParasBisht007@users.noreply.github.com>
2026-02-21 01:22:03 +0530

    blogData.js

--- src/pages/BlogPages/blogData.js
+++ src/pages/BlogPages/blogData.js
@@ -1,34 +1,19 @@
 const blogData = [
-  {
-    id: 4,
-    title: "Fourth Blog",
-    shortDesc: "Short description for blog 3.",
-    image: "/logos/domination_league.webp",
+    {
+    id: 2,
+    title: "Domination League",
+    shortDesc: "The esports scene is growing faster than ever, and with it comes new opportunities for players to prove their skills, build their reputation, and compete at a higher level.",
+    image: "/logos/domination_leauge.webp",
     path: "/blog/domination",
     year: "2026"
   },
-  {
-    id: 3,
-    title: "Third Blog",
-    shortDesc: "Short description for blog 3.",
-    image: "/images/paras.jpg",
-    path: "",
-    year: "2026"
-  },
-  {
-    id: 2,
-    title: "Second Blog",
-    shortDesc: "Short description for blog 2.",
-    image: "/images/paras.jpg",
-    path: "",
-    year: "2026"
-  },
+  
   {
     id: 1,
-    title: "First Blog",
-    shortDesc: "Short description for blog 1.",
-    image: "/images/paras.jpg",
-    path: "",
+    title: "Domination League",
+    shortDesc: "The esports scene is growing faster than ever, and with it comes new opportunities for players to prove their skills, build their reputation, and compete at a higher level.",
+    image: "/logos/domination_leauge.webp",
+    path: "/blog/domination",
     year: "2026"
   },
   
