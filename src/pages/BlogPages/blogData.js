b9e8ed6a6d602a8877e84d5abf1b7b610accd16b
Paras Bisht <96820300+ParasBisht007@users.noreply.github.com>
2026-02-21 01:36:02 +0530

    blogData.js

--- src/pages/BlogPages/blogData.js
+++ src/pages/BlogPages/blogData.js
@@ -10,10 +10,10 @@ const blogData = [
   
   {
     id: 1,
-    title: "Domination League",
-    shortDesc: "The esports scene is growing faster than ever, and with it comes new opportunities for players to prove their skills, build their reputation, and compete at a higher level.",
-    image: "/logos/domination_leauge.webp",
-    path: "/blog/domination",
+    title: "About UK Esports Hub",
+    shortDesc: "UK Esports Hub is an esports organization focused on building competitive opportunities....",
+    image: "/logos/social_logo.png",
+    path: "/blog/ukesportshub",
     year: "2026" 
   }
     
