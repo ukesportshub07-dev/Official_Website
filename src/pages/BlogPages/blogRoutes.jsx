30f4d9591616b56a44e1b6bd1f8cd8f51e136613
Udit Samant_07 <118661713+Udit-07@users.noreply.github.com>
2026-02-21 01:42:39 +0530

    Blog.jsx

--- src/pages/BlogPages/blogRoutes.jsx
+++ src/pages/BlogPages/blogRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Domination from './domination'
+import Ukesportshub from './ukesportshub'
 
 const BlogRoutes = () => {
     return (
         <Routes>
             <Route path="domination" element={<Domination/>} />
+            <Route path="ukesportshub" element={<Ukesportshub/>} />
             <Route path="*" element={<div className="p-10 text-center"><h1 className="text-4xl text-red-500">Blog Page Error</h1><p>Page Not Found</p></div>} />
         </Routes>
     )
