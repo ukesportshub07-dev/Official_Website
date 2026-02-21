9d6e930e3d7886d4b52dab82d993d461c8ddd598
Paras Bisht <96820300+ParasBisht007@users.noreply.github.com>
2026-02-21 23:39:48 +0530

    blogRoutes.jsx

--- src/pages/BlogPages/blogRoutes.jsx
+++ src/pages/BlogPages/blogRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Domination from './domination'
 import Ukesportshub from './ukesportshub'
 import Valorantenc from './valorantenc'
+import Valorantenc from './parivisionmouz'
 
 const BlogRoutes = () => {
     return (
@@ -10,6 +11,7 @@ const BlogRoutes = () => {
             <Route path="domination" element={<Domination/>} />
             <Route path="ukesportshub" element={<Ukesportshub/>} /> 
             <Route path="valorantenc" element={<Valorantenc/>} />
+            <Route path="parivisionmouz" element={<ParivisionMouz/>} /> 
             <Route path="*" element={<div className="p-10 text-center"><h1 className="text-4xl text-red-500">Blog Page Error</h1><p>Page Not Found</p></div>} />
         </Routes>
     )
